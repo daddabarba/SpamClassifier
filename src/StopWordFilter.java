313989297bdbe94e402d7ef7beb7eb33d26a3a9e
daddabarba <davidebarbieri97@gmail.com>
2018-10-09 00:37:50 +0200

    minimum word size is now a parameter of the filter

--- src/StopWordFilter.java
+++ src/StopWordFilter.java
@@ -3,8 +3,18 @@ package src;
 public class StopWordFilter implements Filter<String>{
     ///Filters out stop words (words with length less than 4)
 
+    private int minSize; ///Minimum word size
+
+    public StopWordFilter(int minSize){
+        this.minSize = minSize;
+    }
+
+    public StopWordFilter(){
+        this(4);
+    }
+
     @Override
     public boolean accept(String word){
-        return word.length()>4;
+        return word.length()>this.minSize;
     }    
 }
